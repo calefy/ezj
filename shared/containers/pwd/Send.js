74ecd556feb72ae761cf50ca273ab57f0a1d8a69
xmn <450825205@qq.com>
2016-03-25 10:17:50 +0800

    找回密码

--- shared/containers/pwd/Send.js
+++ shared/containers/pwd/Send.js
@@ -15,11 +15,32 @@ class Pwd extends Component {
     }
 
     state = {
-        
+        errorMsg: null
     };
 
     componentDidMount() {
     }
+
+    onEnter = () => {
+        // 检查输入
+        if (this._validName()) {
+            // this.props.onSubmit({
+            //     username: this.refs.username.getValue().trim(),
+            //     password: this.refs.password.getValue(),
+            //     remember: this.refs.remember.isChecked() ? 1 : 0
+            // });
+            // this._setState({ modified: false }); // 登录请求发出后，重置modified
+            console.log(errorMsg);
+        }
+    };
+    _validName = () => {
+        const val = this.refs.name.getValue().trim();
+        const email = "/^[a-zA-Z0-9_.-]+@[a-zA-Z0-9_-]+(\.[a-zA-Z0-9_-]+)*(\.[a-zA-Z0-9_-]{2,4}){1,2}$/";
+        const mobile = "/^1[3-9]\d{9}$/";
+        this._setState({ errorMsg : val.match(email) || val.match(mobile) ? '' : '账号格式错误' });
+
+        return !!val;
+    };
 
     enableButton = () => {
         this.setState({
@@ -34,6 +55,9 @@ class Pwd extends Component {
             bgColor: 'rgb(229,229,229)'
         });
     };
+    _setState = obj => {
+        this.setState(Object.assign({}, this.state, obj || {}));
+    };
 
     render() {
         return (
@@ -41,15 +65,17 @@ class Pwd extends Component {
                 <Formsy.Form
                     onValid={this.enableButton}
                     onInvalid={this.disableButton}
+                    onValidSubmit={this.onEnter}
                     className="pwd-form pwd-write-form">
                     <FormsyText 
                         name="name" 
+                        ref="name"
                         title="输入注册手机号/邮箱"
                         type="text"
                         required />
                     <div className="pop-btn pwd-btn">
                         <button type="submit" disabled={!this.state.canSubmit} 
-                            className={ this.state.canSubmit ? "" : "disabled"} ><Link to="/pwd/code">发送验证码</Link></button>
+                            className={ this.state.canSubmit ? "" : "disabled"} >发送验证码</button>
                     </div>
                 </Formsy.Form>
             </div>
