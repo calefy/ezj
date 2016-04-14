833a464eca3a51f2b29af20fe0b6b0f7df45d988
陈林飞 <chenlinfei@ezijing.com>
2016-04-14 18:01:25 +0800

    调整help页面显示正常

--- shared/containers/other/Opinion.js
+++ shared/containers/other/Opinion.js
@@ -12,9 +12,7 @@ class Account extends Component {
                 <h4 className="h4-title">意见反馈</h4>
                 <div className="other-opinion">
                     <input type="text" placeholder="我的手机" /><input type="email" placeholder="我的邮箱" />
-                    <textarea>
-                        我要提问或意见反馈
-                    </textarea><br />
+                    <textarea placeholder="我要提问或意见反馈"></textarea><br />
                     <button className="btn" name="" value="提交" type="submit">提交</button>
                 </div>
             </div>
