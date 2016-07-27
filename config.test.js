0a12bc8ea6e0cc5751b35db516e8dce90c076a6e
陈林飞 <chenlinfei@ezijing.com>
2016-07-27 09:07:56 +0800

    接口修改为https调用

--- config.test.js
+++ config.test.js
@@ -1,6 +1,6 @@
 /**
  * 配置文件，需拷贝为同目录下 config.js
  */
-exports.apiPrefix = 'http://api2.ezijing.com/v3';
+exports.apiPrefix = 'https://api2.ezijing.com/v3';
 exports.apiReplacePrefix = '/v3/'; // 要替换的前缀
 exports.autopack = false;
